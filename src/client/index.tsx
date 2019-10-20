48c56746f1e96b5410e9e70d313a1716aea6bd8a
kk-mats <k.mats.kk@gmail.com>
2019-10-20 23:43:36 +0900

    Show sign up form for unauthorized users

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,15 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
-import Home from "client/pages/Home";
-import Register from "client/pages/Register";
+import Register from "client/components/pages/Register";
 
 const LabGo: React.FunctionComponent = () => {
 	return (
 		<BrowserRouter>
-			<Route exact path="/" component={Home} />
-			<Route exact path="/register" component={Register} />
+			<Route path="/" component={Register} />
 		</BrowserRouter>
 	);
 };
